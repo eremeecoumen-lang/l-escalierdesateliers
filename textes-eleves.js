d75ef4112c335d40c21a494af44b3049bfdd491d
eremeecoumen-lang <eremeecoumen@gmail.com>
2026-05-01 20:57:30 +0200

    Update textes-eleves.js

--- textes-eleves.js
+++ textes-eleves.js
@@ -1,16 +1,82 @@
 window.ARCHIVES_TEXTUELLES = [
-  /*
-  Exemple a copier pour ajouter une marche :
-
   {
-    title: "Titre facultatif",
-    createdAt: "2026-05-01T10:00:00.000Z",
-    text: `Colle ici le texte de l'eleve.
-
-Tu peux garder les retours a la ligne.`
+    title: "Marisa et le chien - 01",
+    createdAt: "2026-05-01T12:14:00.000Z",
+    text: `Le chien trottina un peu plus loin, jusqu'a un petit parking borde par un jardin ouvrier desert et couvert par l'ombre de deux grands chenes lieges dont les frondaisons se melangeaient.`
+  },
+  {
+    title: "Marisa et le chien - 02",
+    createdAt: "2026-05-01T12:13:00.000Z",
+    text: `Tout etait calme et vide. On entendait le pepiement d'un oiseau solitaire auquel repondait, de loin en loin, l'echo eparpille du son etouffe d'une tondeuse a gazon et le murmure d'une radio pratiquement inaudible. Il faisait tiede et bleu.`
+  },
+  {
+    title: "Marisa et le chien - 03",
+    createdAt: "2026-05-01T12:12:00.000Z",
+    text: `Marisa s'approcha du chien qui s'etait couche devant le grillage vert qui faisait frontiere entre les herbes folles du jardin et les trois poubelles qui exhalaient une vague odeur de vin, de plastique blanc et de papier brule.`
+  },
+  {
+    title: "Marisa et le chien - 04",
+    createdAt: "2026-05-01T12:11:00.000Z",
+    text: `Le chien avait pose sa grosse tete noire sur ses pattes avant et regardait placidement la vieille femme approcher. Autour de lui, le vent faisait s'agiter une petite fougere morne qui poussait dans cette zone intermediaire depuis la nuit des temps.`
+  },
+  {
+    title: "Marisa et le chien - 05",
+    createdAt: "2026-05-01T12:10:00.000Z",
+    text: `En marchant lentement vers le chien, Marisa pensa que tous les dimanches d'avril etaient la, dans ce coin de ville recule, quelque part au pied de cette poubelle a carton de laquelle s'echappait quelques papiers administratifs couvert de gribouillis.`
+  },
+  {
+    title: "Marisa et le chien - 06",
+    createdAt: "2026-05-01T12:09:00.000Z",
+    text: `Arrive a un metre du chien, Marisa s'accroupit devant lui, plongea ses yeux dans les pupilles obscures du chien et approcha sa main pour le caresser. Une bourrasque plus forte que les autres fit bruisser les mille feuilles du chene.`
+  },
+  {
+    title: "Marisa et le chien - 07",
+    createdAt: "2026-05-01T12:08:00.000Z",
+    text: `"Faites pas ca" dit soudainement une voix dans le dos de Marisa. En se retournant, Marisa ne vit pas l'homme qui avait parle, mais seulement une indiscernable silhouette decoupee sur le fond du ciel, en contre-jour.`
+  },
+  {
+    title: "Marisa et le chien - 08",
+    createdAt: "2026-05-01T12:07:00.000Z",
+    text: `"Il est pas si gentil qu'il en a l'air" ajouta la voix, d'un ton egal. Puis, la silhouette se retourna, fit claquer sa langue une fois pour appeler le chien et s'eloigna, sans rien dire de plus.`
+  },
+  {
+    title: "Marisa et le chien - 09",
+    createdAt: "2026-05-01T12:06:00.000Z",
+    text: `Marisa regarda l'homme tourner a l'angle d'une ruelle ou tout un peuple de petites maisons semblables formait une assemblee muette d'ardoises et de crepis blanchatre. Puis, elle fut de nouveau seule.`
+  },
+  {
+    title: "Marisa et le chien - 10",
+    createdAt: "2026-05-01T12:05:00.000Z",
+    text: `Le vent s'etait calme et, encore accroupie devant le grillage, comme recueillie devant une pierre tombale pratiquement disparue, elle fut presque convaincue avoir deja vecu cet instant, precisement cet instant-la, des centaines de fois depuis sa plus tendre enfance.`
+  },
+  {
+    title: "Marisa et le chien - 11",
+    createdAt: "2026-05-01T12:04:00.000Z",
+    text: `Elle se vit jeune fille, adolescente, puis dans la fleur de l'age, marcher cent fois sur les trottoirs fendus d'un village mort et cent fois trouver, a la fin de sa promenade, trois containers semblables, bouches ouvertes sur l'immense vide d'un lotissement sans ame.`
+  },
+  {
+    title: "Marisa et le chien - 12",
+    createdAt: "2026-05-01T12:03:00.000Z",
+    text: `Elle se redressa et, remarquant une file indienne de fourmis remontant le tronc du chene liege du jardin, ses pensees convergerent encore vers un point recule ou, toute petite enfant, elle s'etait penchee, dans le jardin de sa grand-mere, sur les insectes qui vivaient une existence bizarre sous les souches pourrissantes d'au-dela la grande haie.`
+  },
+  {
+    title: "Marisa et le chien - 13",
+    createdAt: "2026-05-01T12:02:00.000Z",
+    text: `Elle entendit, au loin, a une distance beaucoup trop eloignee pour etre meme possible, le lourd aboiement d'un chien qu'elle imagina, meme si cela n'avait pas de sens, etre le chien noir qu'elle avait poursuivi.`
+  },
+  {
+    title: "Marisa et le chien - 14",
+    createdAt: "2026-05-01T12:01:00.000Z",
+    text: `Enfin, un papillon blanc passa au-dessus d'une des poubelles et alla, de son vol claudiquant, s'ecraser sur tout le bleu horizontal du ciel.`
+  },
+  {
+    title: "Marisa et le chien - 15",
+    createdAt: "2026-05-01T12:00:00.000Z",
+    text: `Alors, elle sentit, plus qu'elle ne reva, qu'Anna Brazier etait la, avec elle, ou plutot qu'elle avait ete la aussi, et qu'elles se savaient presentes, l'une et l'autre en cet endroit curieux, zone intermediaire et pratiquement refractaire a toute vie, par-dela l'espace et toute chronologie.`
+  },
+  {
+    title: "Marisa et le chien - 16",
+    createdAt: "2026-05-01T11:59:00.000Z",
+    text: `Alors seulement, elle se decida a rebrousser chemin pour revenir chez Lucho. Mais, sur tout le chemin du retour, elle fut inquiete et habitee par un pressentiment terrible et impossible a decrire avec les mots du langage courant.`
   }
-
-  Les textes les plus recents doivent etre places en haut de la liste.
-  Chaque bloc est separe du suivant par une virgule.
-  */
 ];
